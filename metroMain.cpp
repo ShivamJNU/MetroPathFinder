b85478c7dbce79f6b76d84081968153fbd88e028
ShivamJNU <119946005+ShivamJNU@users.noreply.github.com>
2024-11-05 01:39:56 +0530

    Update metroMain.cpp

--- metroMain.cpp
+++ metroMain.cpp
@@ -2,6 +2,8 @@
 #include "assignAllLines.cpp"
 #include "assignAllInternalGraph.cpp"
 #include "assignAllTimeGraph.cpp"
+#include "shortestPathTime.cpp"
+#include "shortestPathIntermediateStations.cpp"
 
 using namespace std;
 
@@ -12,132 +14,6 @@ map<int, string> m2;
 
 vector<vector<int>> graphDist;
 vector<vector<int>> graphTime;
-
-vector<int> shortestPathIntermediateStations(int n, int src, int dst)
-{
-
-    vector<pair<int, int>> adj[n + 1];
-    for (auto it : graphDist)
-    {
-        adj[it[0]].push_back({it[1], it[2]});
-        adj[it[1]].push_back({it[0], it[2]});
-    }
-
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-
-    vector<int> dist(n + 1, 1e9), parent(n + 1);
-    for (int i = 1; i <= n; i++)
-    {
-        parent[i] = i;
-    }
-
-    dist[src] = 0;
-
-    pq.push({0, src});
-    while (!pq.empty())
-    {
-
-        auto it = pq.top();
-        pq.pop();
-        int node = it.second;
-        int dis = it.first;
-
-        for (auto it : adj[node])
-        {
-            int adjNode = it.first;
-            int edW = it.second;
-
-            if (dis + edW < dist[adjNode])
-            {
-                dist[adjNode] = dis + edW;
-                pq.push({dis + edW, adjNode});
-
-                parent[adjNode] = node;
-            }
-        }
-    }
-
-    if (dist[dst] == 1e9)
-    {
-        return {-1};
-    }
-
-    vector<int> path;
-    int node = dst;
-
-    while (parent[node] != node)
-    {
-        path.push_back(node);
-        node = parent[node];
-    }
-    path.push_back(src);
-
-    reverse(path.begin(), path.end());
-    return path;
-}
-
-vector<int> shortestPathTime(int n, int src, int dst)
-{
-
-    vector<pair<int, int>> adj[n + 1];
-    for (auto it : graphTime)
-    {
-        adj[it[0]].push_back({it[1], it[2]});
-        adj[it[1]].push_back({it[0], it[2]});
-    }
-
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-
-    vector<int> dist(n + 1, 1e9), parent(n + 1);
-    for (int i = 1; i <= n; i++)
-    {
-        parent[i] = i;
-    }
-
-    dist[src] = 0;
-
-    pq.push({0, src});
-    while (!pq.empty())
-    {
-
-        auto it = pq.top();
-        pq.pop();
-        int node = it.second;
-        int dis = it.first;
-
-        for (auto it : adj[node])
-        {
-            int adjNode = it.first;
-            int edW = it.second;
-
-            if (dis + edW < dist[adjNode])
-            {
-                dist[adjNode] = dis + edW;
-                pq.push({dis + edW, adjNode});
-
-                parent[adjNode] = node;
-            }
-        }
-    }
-
-    if (dist[dst] == 1e9)
-    {
-        return {-1};
-    }
-
-    vector<int> path;
-    int node = dst;
-
-    while (parent[node] != node)
-    {
-        path.push_back(node);
-        node = parent[node];
-    }
-    path.push_back(src);
-
-    reverse(path.begin(), path.end());
-    return path;
-}
 
 signed main()
 {
@@ -173,11 +49,11 @@ signed main()
 
     if (f == 1)
     {
-        path = shortestPathTime(300, m1[src], m1[dst]);
+        path = shortestPathTime(300, m1[src], m1[dst], graphTime);
     }
     else
     {
-        path = shortestPathIntermediateStations(300, m1[src], m1[dst]);
+        path = shortestPathIntermediateStations(300, m1[src], m1[dst], graphDist);
     }
 
     if (path[0] == -1)
